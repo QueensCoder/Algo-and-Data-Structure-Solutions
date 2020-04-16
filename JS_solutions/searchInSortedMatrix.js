7dec7ed90baa77141053409b0c47446d670b4025
Ozal Khan <ozalkhan196@gmail.com>
2020-04-16 12:52:43 -0400

    added comments to sism

--- JS_solutions/searchInSortedMatrix.js
+++ JS_solutions/searchInSortedMatrix.js
@@ -4,6 +4,8 @@
 
 //if number is smaller than target then elimnate numbers to the left
 
+// if pointers move outside of matrix end the search and return -1 , -1 because the matrix does not contain the number
+// (O n + m)
 function searchInSortedMatrix(matrix, target) {
   let row = 0;
   let col = matrix[0].length - 1;
