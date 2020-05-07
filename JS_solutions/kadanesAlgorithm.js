bdf4c3b251dc7e82824032cb2c6f6e42a3421342
Ozal Khan <ozalkhan196@gmail.com>
2020-05-07 13:42:43 -0400

    updated subsetsums to kadans algorithm

--- JS_solutions/kadanesAlgorithm.js
+++ JS_solutions/kadanesAlgorithm.js
@@ -1,10 +1,9 @@
 // find the max subset sum in an array
-// alternative to kadanes algorithm
 
 // iterate through array currSum = max of currSum + num or num
 // if currSum > maxSum set a new maxSum
 
-function maxSubSetSum(array) {
+function kadanesAlgorithm(array) {
   let maxSum = -Infinity;
   let currSum = 0;
 
@@ -15,4 +14,6 @@ function maxSubSetSum(array) {
   return maxSum;
 }
 
-console.log(maxSubSetSum([3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4]));
+console.log(
+  kadanesAlgorithm([3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4])
+); //returns 19
