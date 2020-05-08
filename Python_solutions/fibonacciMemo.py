1df4df8699889608da14cac646fd22e87cf6e599
Ozal Khan <ozalkhan196@gmail.com>
2020-05-08 11:42:58 -0400

    updated fibonacci memo to use lru_cache to handle caching for cleaner code

--- Python_solutions/fibonacciMemo.py
+++ Python_solutions/fibonacciMemo.py
@@ -1,34 +1,15 @@
+from functools import lru_cache
 
 
-
-# without using cache
-# def fibonacci(n):
-#     if n == 1:
-#         return 1
-#     elif n == 2:
-#         return 1
-#     elif n > 2:
-#         return fibonacci(n - 1) + fibonacci(n - 2)
-
-fib_cache = {}
-
+@lru_cache(maxsize=1000)
 def fibonacci(n):
-    # if value is cached return its fib value
-    if n in fib_cache:
-        return fib_cache[n]
-    elif not n:
+    if not n:
         return 0
-    elif n == 0 or  n == 1 or n == 2:
+    elif n == 1 or n == 2:
         return 1
     elif n > 2:
-        value = fibonacci(n - 1) + fibonacci(n - 2)
-
-    fib_cache[n] = value
-    return value
-        
+        return fibonacci(n - 1) + fibonacci(n - 2)
 
 
-
-for n in range(0, 41):
+for n in range(0, 600):
     print(n, ':', fibonacci(n))
-
