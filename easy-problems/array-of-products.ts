69ecea79b26d24003bfa857f0b2f6fff0c97f023
Ozal Khan <ozalkhan196@gmail.com>
2024-12-03 19:14:27 -0500

    chore: solve array of products with opt solution

--- easy-problems/array-of-products.ts
+++ easy-problems/array-of-products.ts
@@ -1,4 +1,4 @@
-export function arrayOfProductsBruteFource(array: number[]) {
+export function arrayOfProductsBruteForce(array: number[]) {
   const res: number[] = [];
   for (let i = 0; i < array.length; i++) {
     let product = 1;
@@ -10,4 +10,25 @@ export function arrayOfProductsBruteFource(array: number[]) {
   return res;
 }
 
-console.log(arrayOfProductsBruteFource([5, 1, 4, 2])); // [8, 40, 10, 20]
+// console.log(arrayOfProductsBruteForce([5, 1, 4, 2])); // [8, 40, 10, 20]
+
+export function arrayOfProducts(array: number[]) {
+  const leftRes: number[] = Array(array.length).fill(1);
+  const rightRes: number[] = Array(array.length).fill(1);
+
+  let leftProduct = 1;
+  for (let i = 0; i < array.length; i++) {
+    leftRes[i] = leftProduct;
+    leftProduct *= array[i];
+  }
+
+  let rightProduct = 1;
+  for (let i = array.length - 1; i >= 0; i--) {
+    rightRes[i] = rightProduct;
+    rightProduct *= array[i];
+  }
+
+  return leftRes.map((_, i) => leftRes[i] * rightRes[i]);
+}
+
+console.log(arrayOfProducts([5, 1, 4, 2])); // [8, 40, 10, 20]
