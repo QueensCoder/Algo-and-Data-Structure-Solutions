7d2c57e98f367e89c8b1bf6b0294c1b721f08aa2
Ozal Khan <ozalkhan196@gmail.com>
2024-12-05 19:49:09 -0500

    chore: add prettier for formatting

--- easy-problems/common-characters.ts
+++ easy-problems/common-characters.ts
@@ -1,4 +1,24 @@
+export function commonCharactersBrute(strings: string[]) {
+  const allChars = new Set(strings.join('').split(''));
+  const totalWords = strings.length;
+  const result: string[] = [];
+
+  allChars.forEach((char) => {
+    const currentMatches = strings.filter((word) => word.includes(char));
+    if (currentMatches.length === totalWords) result.push(char);
+  });
+
+  return result;
+}
+
+console.log(commonCharactersBrute(['abc', 'efg', 'hij'])); // []
+console.log(commonCharactersBrute(['b', 'ccb', 'abc'])); // ['b', 'c']
+
 export function commonCharacters(strings: string[]) {
+  type Hash = {
+    [key: string]: number;
+  };
+  const hash = {};
   const allChars = new Set(strings.join('').split(''));
   const totalWords = strings.length;
   const result: string[] = [];
