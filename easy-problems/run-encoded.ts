95022e4374e8fc991dd07e41123355f529026a01
Ozal Khan <ozalkhan196@gmail.com>
2024-12-09 16:50:28 -0500

    chore: add approach to run-encdoed

--- easy-problems/run-encoded.ts
+++ easy-problems/run-encoded.ts
@@ -1,7 +1,13 @@
 /**
+ * Problem
+ * - For each character in the string, turn AAA into 3A, with a maximum of 9 per character
+ * - greater than 9 becomes another entry ex 12A is represented as 9A3A
+ *
  * Approach
- *
- *
+ * - Initialize an empty string to store the result
+ * - for each letter in the string, store the current letter and the count, dump if 9 and add to result
+ * - if char changes dump and add to result
+ * - dump means remove from current count/storage and add to result string
  */
 
 export function runLengthEncoding(string: string) {
