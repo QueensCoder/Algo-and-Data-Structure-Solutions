4049d32e2552175da9943263756d6f698f1e091a
Ozal Khan <ozalkhan196@gmail.com>
2024-12-09 19:42:36 -0500

    feat: setup binary tree expression logic

---
+++ easy-problems/evaluate-binary-expression-tree.ts
@@ -0,0 +1,17 @@
+// This is an input class. Do not edit.
+export class BinaryTree {
+  value: number;
+  left: BinaryTree | null;
+  right: BinaryTree | null;
+
+  constructor(value: number) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+export function evaluateExpressionTree(tree: BinaryTree) {
+  // Write your code here.
+  return -1;
+}
