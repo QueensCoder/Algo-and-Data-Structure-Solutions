710d20fb7538473a7df37dbce2287c5bd318c952
Ozal Khan <ozalkhan196@gmail.com>
2024-12-11 18:45:51 -0500

    bug: missing last calc

--- easy-problems/ts-solutions/min-waiting-time.ts
+++ easy-problems/ts-solutions/min-waiting-time.ts
@@ -5,7 +5,7 @@ export function minimumWaitingTime(queries: number[]) {
   queries.sort((a, b) => a - b);
   for (let i = 0; i < queries.length; i++) {
     let currNum = queries[i];
-    console.log(currNum);
+
     if (!i) waitTimes.push(0);
     else {
       let previousTimes = queries.slice(0, i);
@@ -21,39 +21,39 @@ export function minimumWaitingTime(queries: number[]) {
 
 //  5     1      4  - query durations
 // (0) + (5) + (5 + 1) = 11 - wait times (note 0 for first query, and last query duration is not used in a wait time)
-console.log(minimumWaitingTime([5, 1, 4]), 11); // 11
+// console.log(minimumWaitingTime([5, 1, 4]), 11); // 11
 
 // 3     2     1     2     6
 // 0   + 3  +  5 +   6  +  8 = 17
 // 0 +   3 +  3+2 +  3+2+1 + 6+3+2+1
-console.log(minimumWaitingTime([3, 2, 1, 2, 6]), 17); // 17
+// console.log(minimumWaitingTime([3, 2, 1, 2, 6]), 17); // 17
 
+// without storing the wait times inside of an array we're using math
+// totalTime += currNum * queries left
+// 1    4    5
+
+// 1 * 3 = 3
+// 4 * 2 = 8
+// 5 * 0 = 0
 export function minimumWaitingTimeOptimizedSpace(queries: number[]) {
-  const waitTimes: number[] = [];
-
+  let prevSum = 0;
+  let totalTime = 0;
   //   move largest queries to the end then calculate the total wait times
   queries.sort((a, b) => a - b);
-  for (let i = 0; i < queries.length; i++) {
-    let currNum = queries[i];
-    console.log(currNum);
-    if (!i) waitTimes.push(0);
-    else {
-      let previousTimes = queries.slice(0, i);
+  queries.forEach((num, idx) => {
+    let queriesLeft = queries.length - (idx + 1);
+    // console.log(queriesLeft);
+    totalTime += num * queriesLeft;
+  });
 
-      waitTimes.push(...previousTimes);
-    }
-  }
-
-  return waitTimes.reduce((acc, curr) => {
-    return acc + curr;
-  }, 0);
+  return totalTime;
 }
 
 //  5     1      4  - query durations
 // (0) + (5) + (5 + 1) = 11 - wait times (note 0 for first query, and last query duration is not used in a wait time)
-console.log(minimumWaitingTimeOptimizedSpace([5, 1, 4]), 11); // 11
+console.log(minimumWaitingTimeOptimizedSpace([5, 1, 4])); // 11
 
 // 3     2     1     2     6
 // 0   + 3  +  5 +   6  +  8 = 17
 // 0 +   3 +  3+2 +  3+2+1 + 6+3+2+1
-console.log(minimumWaitingTimeOptimizedSpace([3, 2, 1, 2, 6]), 17); // 17
+// console.log(minimumWaitingTimeOptimizedSpace([3, 2, 1, 2, 6]), 17); // 17
