59f2e3cb488feb0445b6a49fc06a5957586e62f2
Ozal Khan <ozalkhan196@gmail.com>
2024-12-17 18:54:08 -0500

    chore: use new approach using helpers

--- frontend-expert/flatten.js
+++ frontend-expert/flatten.js
@@ -1,39 +1,43 @@
-function flatten(value) {
-  let flattendObject = {};
-  const flattenedArray = [];
-
-  // if (typeof value !== 'object' && !Array.isArray(value)) {
-  //   return value;
-  // }
-
-  //non nested examples
-  if (Array.isArray(value)) {
-    for (let i = 0; i < value.length; i++) {
-      let currentValue = value[i];
-      if (!Array.isArray(currentValue)) {
-        flattenedArray.push(currentValue);
-      } else {
-        let recursed = flatten(currentValue);
-        flattenedArray.push(...recursed);
-      }
+function flattenArray(arr) {
+  if (arr === null || !Array.isArray(arr)) return arr;
+  return arr.reduce((acc, item) => {
+    if (Array.isArray(item)) {
+      return acc.concat(flattenArray(item));
+    } else if (!Array.isArray(item) && typeof item === 'object') {
+      return acc.concat(flattenObject(item));
     }
-
-    return flattenedArray;
-  }
-
-  if (typeof value === 'object') {
-    for (let key in value) {
-      let currentValue = value[key];
-      if (typeof currentValue !== 'object' && !Array.isArray(currentValue)) {
-        flattendObject[key] = currentValue;
-      } else {
-        flattendObject = { ...flattendObject, ...flatten(currentValue) };
-      }
-    }
-    return flattendObject;
-  }
+    return acc.concat(item);
+  }, []);
 }
 
+function flattenObject(obj) {
+  let result = {};
+  if (obj === null || typeof obj !== 'object') return obj;
+
+  for (const key in obj) {
+    let value = obj[key];
+    if (!Array.isArray(value) && typeof value === 'object') {
+      let flattendObj = flattenObject(value);
+      result = { ...result, ...flattendObj };
+    } else if (Array.isArray(value)) {
+      let flattendArr = flattenArray(value);
+      result = { ...result, [key]: flattendArr };
+    } else {
+      result[key] = value;
+    }
+  }
+  return result;
+}
+
+function flatten(value) {
+  if (typeof value !== 'object' || value === null) {
+    return value;
+  }
+
+  if (Array.isArray(value)) return flattenArray(value);
+
+  return flattenObject(value);
+}
 // Do not edit the line below.
 console.log(flatten([1, [2, 3], 4, [5, [6]]])); // [1, 2, 3, 4, 5, 6]
 // console.log(flatten([1, 2]), 'test'); // [1, 2, 3, 4, 5, 6]
