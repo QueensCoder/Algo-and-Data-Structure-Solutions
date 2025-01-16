fdc70592b38c7e143b5bf77bcbf4fc2feeb3e439
Ozal Khan <ozalkhan196@gmail.com>
2025-01-16 17:54:22 -0500

    feat: solve zero sum brute force

--- medium-problems/zero-sum-sub-array.ts
+++ medium-problems/zero-sum-sub-array.ts
@@ -1,4 +1,14 @@
+// brute force solution O(n^2) time, O(1) space
 export function zeroSumSubarray(nums: number[]) {
-  // Write your code here.
+  if (nums.includes(0)) return true;
+  for (let i = 0; i < nums.length; i++) {
+    let currentSum = nums[i];
+    for (let j = i + 1; j < nums.length; j++) {
+      currentSum += nums[j];
+      if (currentSum === 0) return true;
+    }
+  }
   return false;
 }
+
+console.log(zeroSumSubarray([-5, -5, 2, 3, -2]));
