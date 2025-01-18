a79e2142d9d85ac4785fe29eea190a0d28de5676
Ozal Khan <ozalkhan196@gmail.com>
2025-01-18 12:47:08 -0500

    feat: solve zero sum with optimal solution

--- medium-problems/zero-sum-sub-array.ts
+++ medium-problems/zero-sum-sub-array.ts
@@ -11,4 +11,25 @@ export function zeroSumSubarray(nums: number[]) {
   return false;
 }
 
-console.log(zeroSumSubarray([-5, -5, 2, 3, -2]));
+// console.log(zeroSumSubarray([-5, -5, 2, 3, -2]));
+
+// O(n) time, O(n) space, looking at this if a number is ever seen twice that means the nums between the cycles zero'd out
+export const zeroSumSubarrayOptimal = (nums: number[]) => {
+  const sums: number[] = [];
+  let sum = 0;
+  const seen = new Set();
+
+  if (nums.includes(0)) return true;
+
+  for (let i = 0; i < nums.length; i++) {
+    let num = nums[i];
+    sum += num;
+    sums.push(sum);
+    if (seen.has(sum)) return true;
+    else seen.add(sum);
+  }
+  return false;
+};
+
+console.log(zeroSumSubarrayOptimal([-5, -5, 2, 3, -2]));
+console.log(zeroSumSubarrayOptimal([2, -2]));
