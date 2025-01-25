952f3c1d624ebdda588e90c9baca342cda72b9a4
Ozal Khan <ozalkhan196@gmail.com>
2025-01-24 19:02:30 -0500

    chore: new attempt using NaN mark visited

--- medium-problems/single-cycle.ts
+++ medium-problems/single-cycle.ts
@@ -1,22 +1,20 @@
 export function hasSingleCycle(array: number[]) {
-  // list visted indexes
   const arrLen = array.length;
-  const visited = new Set();
   let count = 0;
   let i = 0;
 
-  // if result is negative we need to use a reverse idx by total len + negative idx to get regular idx
-
-  while (visited.size !== arrLen) {
+  while (!isNaN(array[i])) {
     // mark that index was visted
-    visited.add(i);
     let curr = array[i];
+    array[i] = NaN;
     let rawIdxCalc = (i + curr) % arrLen;
+    // if result is negative we need to use a reverse idx by total len + negative idx to get regular idx
     const nextIdx = rawIdxCalc >= 0 ? rawIdxCalc : rawIdxCalc + arrLen;
     i = nextIdx;
+    count++;
   }
 
-  if (visited.size === arrLen && i === 0) return true;
+  if (count === arrLen) return true;
   else return false;
 }
 
