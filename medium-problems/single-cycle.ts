18035aaa837748e63b48e881cbef06df08e80f37
Ozal Khan <ozalkhan196@gmail.com>
2025-01-24 19:05:35 -0500

    feat: solve single cycle optimally

--- medium-problems/single-cycle.ts
+++ medium-problems/single-cycle.ts
@@ -1,3 +1,4 @@
+// O(n) time O(1) space, reuses indexes in the array to mark what is visited
 export function hasSingleCycle(array: number[]) {
   const arrLen = array.length;
   let count = 0;
@@ -14,9 +15,10 @@ export function hasSingleCycle(array: number[]) {
     count++;
   }
 
-  if (count === arrLen) return true;
+  if (count === arrLen && i === 0) return true;
   else return false;
 }
 
 // 2(start point), 1, -4, 2(end 2), 3, -4 (second), 2 (start point)
 console.log(hasSingleCycle([2, 3, 1, -4, -4, 2])); // true
+console.log(hasSingleCycle([1, 1, 1, 1, 2]));
