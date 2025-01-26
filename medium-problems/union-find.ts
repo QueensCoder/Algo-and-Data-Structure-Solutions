0d2694d1753b1f542cbc8430a83f2cb8c21a7612
Ozal Khan <ozalkhan196@gmail.com>
2025-01-26 14:40:14 -0500

    chore: setup union find

---
+++ medium-problems/union-find.ts
@@ -0,0 +1,17 @@
+export class UnionFind {
+  constructor() {
+    // Write your code here.
+  }
+
+  createSet(value: number) {
+    // Write your code here.
+  }
+
+  find(value: number) {
+    // Write your code here.
+  }
+
+  union(valueOne: number, valueTwo: number) {
+    // Write your code here.
+  }
+}
