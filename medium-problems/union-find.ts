0f727e0948259ff9d50951f21247ec2e5ef5297b
Ozal Khan <ozalkhan196@gmail.com>
2025-01-27 15:32:45 -0500

    feat: setup find and union methods

--- medium-problems/union-find.ts
+++ medium-problems/union-find.ts
@@ -1,16 +1,22 @@
 export class UnionFind {
-  tree: { [key: number]: {} } = {};
+  tree: { [key: number]: Set<number> } = {};
   constructor() {
     this.tree = {};
   }
 
   createSet(value: number) {
     this.tree[value] = new Set();
-    return this.tree[value];
+    return null;
   }
 
   find(value: number) {
-    // Write your code here.
+    if (this.tree[value]) return value;
+    else {
+      const foundKey = Object.keys(this.tree).find((key) =>
+        this.tree[key].has(value),
+      );
+      return foundKey;
+    }
   }
 
   union(valueOne: number, valueTwo: number) {
@@ -18,8 +24,14 @@ export class UnionFind {
     const unionTwo = this.tree[valueTwo];
 
     console.log(unionOne, unionTwo);
-    // this.tree[valueOne] = { ...unionOne, unionTwo, valueTwo };
-    return this.tree[valueOne];
+    if (!unionTwo) {
+      unionOne.add(valueTwo);
+    } else {
+      this.tree[valueOne] = new Set([...unionOne, ...unionTwo, valueTwo]);
+      delete this.tree[valueTwo];
+    }
+
+    return null;
   }
 }
 
@@ -27,7 +39,20 @@ const unionFind = new UnionFind();
 console.log(unionFind.createSet(3));
 console.log(unionFind.createSet(6));
 console.log(unionFind.union(3, 6));
-// console.log(unionFind.find(6));
+console.log(unionFind.find(6));
 // console.log(unionFind.find(3));
 // console.log(unionFind.createSet(2));
 // console.log(unionFind.union(2, 6));
+
+// const testSet = new Set();
+// const testSet2 = new Set();
+// testSet.add(1);
+// testSet.add(2);
+// testSet2.add(3);
+
+// console.log(testSet);
+// console.log(testSet2);
+
+// const newSet = new Set(testSet, testSet2);
+
+// console.log(newSet, '2222');
