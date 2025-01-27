75d99cb005a194693f3355d002aef932d1a88da9
Ozal Khan <ozalkhan196@gmail.com>
2025-01-27 15:16:02 -0500

    feat: add LL construction

--- medium-problems/union-find.ts
+++ medium-problems/union-find.ts
@@ -1,10 +1,12 @@
 export class UnionFind {
+  tree: { [key: number]: {} } = {};
   constructor() {
-    // Write your code here.
+    this.tree = {};
   }
 
   createSet(value: number) {
-    // Write your code here.
+    this.tree[value] = new Set();
+    return this.tree[value];
   }
 
   find(value: number) {
@@ -12,6 +14,20 @@ export class UnionFind {
   }
 
   union(valueOne: number, valueTwo: number) {
-    // Write your code here.
+    const unionOne = this.tree[valueOne];
+    const unionTwo = this.tree[valueTwo];
+
+    console.log(unionOne, unionTwo);
+    // this.tree[valueOne] = { ...unionOne, unionTwo, valueTwo };
+    return this.tree[valueOne];
   }
 }
+
+const unionFind = new UnionFind();
+console.log(unionFind.createSet(3));
+console.log(unionFind.createSet(6));
+console.log(unionFind.union(3, 6));
+// console.log(unionFind.find(6));
+// console.log(unionFind.find(3));
+// console.log(unionFind.createSet(2));
+// console.log(unionFind.union(2, 6));
