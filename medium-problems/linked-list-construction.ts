b884d37c0f202763659f5a0229796a1c2447c619
Ozal Khan <ozalkhan196@gmail.com>
2025-01-28 17:41:46 -0500

    chore: setup LL test specs

--- medium-problems/linked-list-construction.ts
+++ medium-problems/linked-list-construction.ts
@@ -37,3 +37,17 @@ export class DoublyLinkedList {
     return false;
   }
 }
+
+const doublyLinkedList = new DoublyLinkedList();
+const node1 = new Node(1);
+const node2 = new Node(2);
+const node3 = new Node(3);
+const node4 = new Node(4);
+const node5 = new Node(5);
+
+const node6 = new Node(6);
+const node7 = new Node(7);
+const node8 = new Node(8);
+
+// doublyLinkedList.setHead(node1);
+// doublyLinkedList.setTail(node3);
