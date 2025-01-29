f2daaf6749c014bbafd4fb55c9e379c67691ffed
Ozal Khan <ozalkhan196@gmail.com>
2025-01-28 21:28:08 -0500

    feat: solve contains

--- medium-problems/linked-list-construction.ts
+++ medium-problems/linked-list-construction.ts
@@ -34,11 +34,16 @@ export class DoublyLinkedList {
   remove(node: Node) {}
 
   containsNodeWithValue(value: number) {
+    let currNode = this.head;
+    while (currNode) {
+      if (currNode.value === value) return true;
+      currNode = currNode.next;
+    }
     return false;
   }
 }
 
-const doublyLinkedList = new DoublyLinkedList();
+// nodes
 const node1 = new Node(1);
 const node2 = new Node(2);
 const node3 = new Node(3);
@@ -49,5 +54,21 @@ const node6 = new Node(6);
 const node7 = new Node(7);
 const node8 = new Node(8);
 
+// doubly linked list configured manually
+const doublyLinkedList = new DoublyLinkedList();
+doublyLinkedList.head = node1;
+doublyLinkedList.tail = node5;
+node1.next = node2;
+node2.prev = node1;
+node2.next = node3;
+node3.prev = node2;
+node3.next = node4;
+node4.prev = node3;
+node4.next = node5;
+node5.prev = node4;
+
+console.log(doublyLinkedList.containsNodeWithValue(5));
+console.log(doublyLinkedList.containsNodeWithValue(1));
+console.log(doublyLinkedList.containsNodeWithValue(22));
 // doublyLinkedList.setHead(node1);
 // doublyLinkedList.setTail(node3);
