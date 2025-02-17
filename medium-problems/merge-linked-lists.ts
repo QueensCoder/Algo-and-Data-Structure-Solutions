2cee1550758a1047eb41cc0fbd9d36a14c1002f2
Ozal Khan <ozalkhan196@gmail.com>
2025-02-17 16:23:45 -0500

    chore: add merge LL

---
+++ medium-problems/merge-linked-lists.ts
@@ -0,0 +1,18 @@
+// This is an input class. Do not edit.
+export class LinkedList {
+  value: number;
+  next: LinkedList | null;
+
+  constructor(value: number) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+export function mergingLinkedLists(
+  linkedListOne: LinkedList,
+  linkedListTwo: LinkedList,
+) {
+  // Write your code here.
+  return null;
+}
