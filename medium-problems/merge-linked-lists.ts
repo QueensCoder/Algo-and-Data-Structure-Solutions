52a8423dfe78a700058b83f55851ffad05c3b06f
Ozal Khan <ozalkhan196@gmail.com>
2025-02-17 16:26:22 -0500

    chore: setup test cases

--- medium-problems/merge-linked-lists.ts
+++ medium-problems/merge-linked-lists.ts
@@ -16,3 +16,13 @@ export function mergingLinkedLists(
   // Write your code here.
   return null;
 }
+
+const linkedListOne = new LinkedList(2);
+linkedListOne.next = new LinkedList(3);
+linkedListOne.next.next = new LinkedList(1);
+linkedListOne.next.next.next = new LinkedList(4);
+
+const linkedListTwo = new LinkedList(8);
+linkedListTwo.next = new LinkedList(7);
+linkedListTwo.next.next = new LinkedList(1);
+linkedListTwo.next.next.next = new LinkedList(4);
