293294f83b5a34ffa1503fe9714e3bb469c8aa76
Ozal Khan <ozalkhan196@gmail.com>
2025-02-18 21:15:27 -0500

    chore: add height balanced bst

---
+++ medium-problems/height-balance-bst.ts
@@ -0,0 +1,16 @@
+export class BinaryTree {
+  value: number;
+  left: BinaryTree | null;
+  right: BinaryTree | null;
+
+  constructor(value: number) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+export function heightBalancedBinaryTree(tree: BinaryTree) {
+  // Write your code here.
+  return false;
+}
