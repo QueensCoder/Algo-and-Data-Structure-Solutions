018666abe5b9a290c9a4131962ee61839f3de4ec
Ozal Khan <ozalkhan196@gmail.com>
2025-02-19 21:46:19 -0500

    feat: solve for most edge cases

--- medium-problems/height-balance-bst.ts
+++ medium-problems/height-balance-bst.ts
@@ -10,8 +10,41 @@ export class BinaryTree {
   }
 }
 
+const tree = new BinaryTree(1);
+tree.left = new BinaryTree(2);
+tree.right = new BinaryTree(3);
+tree.left.left = new BinaryTree(4);
+tree.left.right = new BinaryTree(5);
+tree.right.right = new BinaryTree(6);
+tree.left.right.left = new BinaryTree(7);
+tree.left.right.right = new BinaryTree(8);
+
+// tree example
+//         1
+//        / \
+//       2   3
+//      / \   \
+//     4   5   6
+//        / \
+//       7   8
+
+export const treeHeightChecker = (tree: BinaryTree | null): number => {
+  if (!tree) return 0;
+  const leftSubTreeHeight = treeHeightChecker(tree.left);
+  const rightSubTreeHeight = treeHeightChecker(tree.right);
+
+  if (Math.abs(leftSubTreeHeight - rightSubTreeHeight) > 1) return NaN;
+
+  return 1 + Math.max(leftSubTreeHeight, rightSubTreeHeight);
+};
+
 export function heightBalancedBinaryTree(tree: BinaryTree) {
-  // Write your code here.
-  return false;
-  // test
+  const leftSubTreeHeight = treeHeightChecker(tree.left);
+  const rightSubTreeHeight = treeHeightChecker(tree.right);
+
+  if (isNaN(leftSubTreeHeight) || isNaN(rightSubTreeHeight)) return false;
+
+  return true;
 }
+
+console.log(heightBalancedBinaryTree(tree)); // true
