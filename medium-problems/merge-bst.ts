3b7148f4f9a85c467540e98a16608aa1db06afdb
Ozal Khan <ozalkhan196@gmail.com>
2025-02-20 18:51:26 -0500

    chore: setup merge bst file

---
+++ medium-problems/merge-bst.ts
@@ -0,0 +1,18 @@
+// This is an input class. Do not edit.
+export class BinaryTree {
+  value: number;
+  left: BinaryTree | null;
+  right: BinaryTree | null;
+
+  constructor(value: number) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+export function mergeBinaryTrees(tree1: BinaryTree, tree2: BinaryTree) {
+  // Write your code here.
+  return null;
+}
+å;
