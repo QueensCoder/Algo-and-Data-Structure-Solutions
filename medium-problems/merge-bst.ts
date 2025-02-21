a858fdeb97f78fedb288c1603a3a6de2e3a08bf7
Ozal Khan <ozalkhan196@gmail.com>
2025-02-21 10:54:13 -0500

    chore: setup approach and tree diagrams

--- medium-problems/merge-bst.ts
+++ medium-problems/merge-bst.ts
@@ -10,20 +10,48 @@ export class BinaryTree {
   }
 }
 
+// We're going to merge tree2 into tree1, so we can just modify tree1 in place
 export const mergeBSTHelper = (
   tree1: BinaryTree | null,
   tree2: BinaryTree | null,
 ) => {
-  const treeOneVal = tree1?.value;
-  const treeTwoVal = tree2?.value;
-  mergeBSTHelper(tree1.left, tree2.left);
-
-  if (treeOneVal && treeTwoVal) tree1.value = treeOneVal + treeTwoVal;
-  if (!treeOneVal) tree1.value = treeTwoVal;
-  mergeBSTHelper(tree1.right, tree2.right);
+  if (!tree1 && !tree2) return;
+  mergeBSTHelper(tree1?.left ?? null, null);
+  console.log(tree1?.value);
+  mergeBSTHelper(tree1?.right ?? null, null);
 };
 
 export function mergeBinaryTrees(tree1: BinaryTree, tree2: BinaryTree) {
-  // Write your code here.
-  return null;
+  mergeBSTHelper(tree1, tree2);
+  return tree1;
 }
+
+// tree example
+// tree1
+//         1
+//        / \
+//       3   2
+//      / \
+//     7   4
+//
+
+// tree2
+//         1
+//        / \
+//       5   9
+//      /   / \
+//     2   7   6
+//
+
+const tree1 = new BinaryTree(1);
+tree1.left = new BinaryTree(3);
+tree1.right = new BinaryTree(2);
+tree1.left.left = new BinaryTree(7);
+tree1.left.right = new BinaryTree(4);
+
+const tree2 = new BinaryTree(1);
+tree2.left = new BinaryTree(5);
+tree2.right = new BinaryTree(9);
+tree2.left.left = new BinaryTree(2);
+tree2.right.left = new BinaryTree(7);
+tree2.right.right = new BinaryTree(6);
