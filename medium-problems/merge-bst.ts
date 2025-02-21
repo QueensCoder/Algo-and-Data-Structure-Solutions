3b375122ef8a9ceb518d6f3bcab90b64f7d6e511
Ozal Khan <ozalkhan196@gmail.com>
2025-02-21 12:56:18 -0500

    feat: solve merge bst optimally

--- medium-problems/merge-bst.ts
+++ medium-problems/merge-bst.ts
@@ -10,20 +10,35 @@ export class BinaryTree {
   }
 }
 
+const dfs = (tree: BinaryTree, arr: number[]) => {
+  if (tree.left) dfs(tree.left, arr);
+  arr.push(tree.value);
+  if (tree.right) dfs(tree.right, arr);
+};
+
+const expectedNodesOrdered = [9, 8, 4, 2, 1, 7, 11, 6];
+
 // We're going to merge tree2 into tree1, so we can just modify tree1 in place
-export const mergeBSTHelper = (
-  tree1: BinaryTree | null,
-  tree2: BinaryTree | null,
-) => {
-  if (!tree1 && !tree2) return;
-  mergeBSTHelper(tree1?.left ?? null, null);
-  console.log(tree1?.value);
-  mergeBSTHelper(tree1?.right ?? null, null);
+export const mergeBSTHelper = (tree1: BinaryTree, tree2: BinaryTree | null) => {
+  if (tree1.left) mergeBSTHelper(tree1.left, tree2?.left ?? null);
+  else if (!tree1.left && tree2?.left) tree1.left = tree2.left;
+  let treeOneValue = tree1.value;
+  let treeTwoValue = tree2?.value;
+  tree1.value = treeOneValue + (treeTwoValue || 0);
+
+  if (tree1.right) mergeBSTHelper(tree1.right, tree2?.right ?? null);
+  else if (!tree1.right && tree2?.right) tree1.right = tree2.right;
+
+  //   if (!tree1?.left && tree2?.left) tree1.left = tree2?.left;
+  //   if (!tree1?.right && tree2?.right) tree1.right = tree2?.right;
 };
 
 export function mergeBinaryTrees(tree1: BinaryTree, tree2: BinaryTree) {
   mergeBSTHelper(tree1, tree2);
-  return tree1;
+  const arr = [];
+  dfs(tree1, arr);
+  console.log(arr, expectedNodesOrdered);
+  //   return tree1;
 }
 
 // tree example
@@ -43,6 +58,13 @@ export function mergeBinaryTrees(tree1: BinaryTree, tree2: BinaryTree) {
 //     2   7   6
 //
 
+// tree1 after merging
+//         2
+//        / \
+//       8   11
+//      / \  / \
+//     9  4  7   6
+
 const tree1 = new BinaryTree(1);
 tree1.left = new BinaryTree(3);
 tree1.right = new BinaryTree(2);
@@ -55,3 +77,5 @@ tree2.right = new BinaryTree(9);
 tree2.left.left = new BinaryTree(2);
 tree2.right.left = new BinaryTree(7);
 tree2.right.right = new BinaryTree(6);
+
+console.log(mergeBinaryTrees(tree1, tree2));
