54e349d8cf1a0c9e06482555478741de2e2a2cc3
Ozal Khan <ozalkhan196@gmail.com>
2025-02-20 19:17:12 -0500

    feat: setup partial solution;

--- medium-problems/merge-bst.ts
+++ medium-problems/merge-bst.ts
@@ -1,4 +1,3 @@
-// This is an input class. Do not edit.
 export class BinaryTree {
   value: number;
   left: BinaryTree | null;
@@ -11,8 +10,20 @@ export class BinaryTree {
   }
 }
 
+export const mergeBSTHelper = (
+  tree1: BinaryTree | null,
+  tree2: BinaryTree | null,
+) => {
+  const treeOneVal = tree1?.value;
+  const treeTwoVal = tree2?.value;
+  mergeBSTHelper(tree1.left, tree2.left);
+
+  if (treeOneVal && treeTwoVal) tree1.value = treeOneVal + treeTwoVal;
+  if (!treeOneVal) tree1.value = treeTwoVal;
+  mergeBSTHelper(tree1.right, tree2.right);
+};
+
 export function mergeBinaryTrees(tree1: BinaryTree, tree2: BinaryTree) {
   // Write your code here.
   return null;
 }
-å;
