a03859084e4ea769a65570d0ec5c4e58c825f8bc
Ozal Khan <ozalkhan196@gmail.com>
2025-02-22 15:05:35 -0500

    chore: setup BST diameter test case

--- medium-problems/bst-diameter.ts
+++ medium-problems/bst-diameter.ts
@@ -14,3 +14,24 @@ export function binaryTreeDiameter(tree: BinaryTree) {
   // Write your code here.
   return -1;
 }
+
+const root = new BinaryTree(1);
+root.left = new BinaryTree(3);
+root.left.left = new BinaryTree(7);
+root.left.left.left = new BinaryTree(8);
+root.left.left.left.left = new BinaryTree(9);
+root.left.right = new BinaryTree(4);
+root.left.right.right = new BinaryTree(5);
+root.left.right.right.right = new BinaryTree(6);
+root.right = new BinaryTree(2);
+
+// tree
+//          1
+//        /   \
+//       3     2
+//      / \
+//     7   4
+//    /     \
+//   8       5
+//  /         \
+// 9           6
