a1c37d839f9e02053e87a27a56a949d102fb9e84
Ozal Khan <ozalkhan196@gmail.com>
2025-02-28 16:19:22 -0500

    feat: solve most cases using optimal solution

--- medium-problems/edit-one.ts
+++ medium-problems/edit-one.ts
@@ -1,8 +1,36 @@
-export function oneEditBruteForce(stringOne: string, stringTwo: string) {}
+export function oneEdit(stringOne: string, stringTwo: string) {
+  let diffs = 0;
 
-console.log(oneEditBruteForce('hello', 'hallo'));
-console.log(oneEditBruteForce('a', 'a'));
-console.log(oneEditBruteForce('abc', 'b'));
+  let stringOnePointer = 0;
+  let stringTwoPointer = 0;
+
+  const maxChars =
+    stringOne.length >= stringTwo.length ? stringOne.length : stringTwo.length;
+
+  //   if the shorter string has too few characters it automatically false
+
+  while (stringOnePointer < maxChars || stringTwoPointer < maxChars) {
+    let longerWordChar = stringOne[stringOnePointer];
+    let shorterWordChar = stringTwo[stringTwoPointer];
+
+    console.log(longerWordChar, shorterWordChar);
+
+    if (longerWordChar !== shorterWordChar) {
+      // difference found
+      diffs++;
+    }
+
+    stringOnePointer++;
+    stringTwoPointer++;
+  }
+
+  // max number of differences allowed are 1
+  return diffs <= 1;
+}
+
+// console.log(oneEditBruteForce('hello', 'hallo'));
+// console.log(oneEditBruteForce('a', 'a'));
+// console.log(oneEditBruteForce('abc', 'b'));
 console.log(
   oneEditBruteForce('abdefghijklmnopqrstuvwxyz', 'bcdefghijklmnopqrstuvwxyz'),
   'should be false',
