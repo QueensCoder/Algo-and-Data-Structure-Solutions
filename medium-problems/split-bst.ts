09b71843eed38a1c3e38d97f4a9edbb075bebe65
Ozal Khan <ozalkhan196@gmail.com>
2025-02-28 16:31:37 -0500

    chore: add split BST

---
+++ medium-problems/split-bst.ts
@@ -0,0 +1,15 @@
+export class BinaryTree {
+  value: number;
+  left: BinaryTree | null;
+  right: BinaryTree | null;
+
+  constructor(value: number) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+export function splitBinaryTree(tree: BinaryTree) {
+  return -1;
+}
