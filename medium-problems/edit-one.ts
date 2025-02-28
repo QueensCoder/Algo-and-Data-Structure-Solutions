a3b98f8c0d8591467d313258cbe76f132a52889e
Ozal Khan <ozalkhan196@gmail.com>
2025-02-28 15:20:12 -0500

    feat attempt new approach edit-one

--- medium-problems/edit-one.ts
+++ medium-problems/edit-one.ts
@@ -1,36 +1,9 @@
-const getCharCount = (word: string) => {
-  return word
-    .split('')
-    .reduce((prev: { [key: string]: number }, curr: string) => {
-      if (prev[curr]) prev[curr] += 1;
-      else prev[curr] = 1;
-      return prev;
-    }, {});
-};
+export function oneEditBruteForce(stringOne: string, stringTwo: string) {}
 
-export function oneEditBruteForce(stringOne: string, stringTwo: string) {
-  const stringOneHash = getCharCount(stringOne);
-  const stringTwoHash = getCharCount(stringTwo);
-
-  const maxDifferences = 1;
-  let currDifferences = 0;
-
-  const longer =
-    stringOne.length >= stringTwo.length ? stringOneHash : stringTwoHash;
-  const shorter =
-    stringOne.length < stringTwo.length ? stringOneHash : stringTwoHash;
-
-  for (let key in longer) {
-    let longerValue = longer[key];
-    let shorterValue = shorter[key];
-    console.log(longerValue, shorterValue, key);
-    if (currDifferences > maxDifferences) return false;
-
-    if (longerValue !== shorterValue) currDifferences++;
-  }
-  return maxDifferences >= currDifferences;
-}
-
-// console.log(oneEditBruteForce('hello', 'hallo'));
-// console.log(oneEditBruteForce('a', 'a'));
+console.log(oneEditBruteForce('hello', 'hallo'));
+console.log(oneEditBruteForce('a', 'a'));
 console.log(oneEditBruteForce('abc', 'b'));
+console.log(
+  oneEditBruteForce('abdefghijklmnopqrstuvwxyz', 'bcdefghijklmnopqrstuvwxyz'),
+  'should be false',
+);
