f830d477ae8c5062cf5b90a8c9463e7f0afa8463
Ozal Khan <ozalkhan196@gmail.com>
2025-03-02 14:50:50 -0500

    chore: setup split bst test case

--- medium-problems/split-bst.ts
+++ medium-problems/split-bst.ts
@@ -11,5 +11,33 @@ export class BinaryTree {
 }
 
 export function splitBinaryTree(tree: BinaryTree) {
-  return -1;
+  let treeTotal = 0;
+
+  const dfs = (tree) => {
+    if (tree.left) dfs(tree.left);
+    treeTotal += treeTotal;
+    if (tree.right) dfs(tree.right);
+  };
+
+  console.log(treeTotal);
 }
+
+const root = new BinaryTree(1);
+root.left = new BinaryTree(3);
+root.right = new BinaryTree(-2);
+root.left.left = new BinaryTree(6);
+root.left.right = new BinaryTree(-5);
+root.left.left.left = new BinaryTree(2);
+root.right.left = new BinaryTree(5);
+root.right.right = new BinaryTree(2);
+
+// tree
+//          1
+//        /   \
+//       3    -2
+//      / \   / \
+//     6  -5 5   2
+//    /
+//   2
+
+console.log(splitBinaryTree(root));
