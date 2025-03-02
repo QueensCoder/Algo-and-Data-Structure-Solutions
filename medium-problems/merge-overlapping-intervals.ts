ce85d33674dfb9b389cf1aaefb6b1a64f2df29a3
Ozal Khan <ozalkhan196@gmail.com>
2025-03-02 17:05:59 -0500

    chore: setup merge overlapping intervals

---
+++ medium-problems/merge-overlapping-intervals.ts
@@ -0,0 +1,15 @@
+export function mergeOverlappingIntervals(array: number[][]) {
+  // Write your code here.
+  console.log(array);
+  return array;
+}
+
+const array = [
+  [1, 2],
+  [3, 5],
+  [4, 7],
+  [6, 8],
+  [9, 10],
+];
+
+console.log(mergeOverlappingIntervals(array));
