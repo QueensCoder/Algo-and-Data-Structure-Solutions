808b10bd7f4f519185f5f9a34e546d0c97bb611f
Ozal Khan <ozalkhan196@gmail.com>
2025-03-02 19:32:05 -0500

    feat: solve most cases merge overlap

--- medium-problems/merge-overlapping-intervals.ts
+++ medium-problems/merge-overlapping-intervals.ts
@@ -1,6 +1,41 @@
 export function mergeOverlappingIntervals(array: number[][]) {
-  // Write your code here.
-  console.log(array);
+  //   first sort the array so we can start examining what we can merge
+  array.sort((a, b) => a[0] - b[0]);
+
+  let currPointer = 0;
+  let nextPointer = 1;
+  let currInterval = array[currPointer];
+  let nextInterval = array[nextPointer];
+
+  while (nextInterval) {
+    let currOne = currInterval[0];
+    let currTwo = currInterval[1];
+
+    let nextOne = nextInterval[0];
+    let nextTwo = nextInterval[1];
+
+    //   look at i and i + 1, if aka curr, next
+    // if curr[1] between next[0] and next[1] AND next[0] between curr[0] and curr[0] merge
+    // else continue and move pointers forward
+    if (
+      nextOne >= currOne &&
+      nextOne <= currTwo &&
+      currTwo >= nextOne &&
+      currTwo <= nextTwo
+    ) {
+      let mergedInterval = [currOne, nextTwo];
+      array.splice(currPointer, 2, mergedInterval);
+
+      currInterval = array[currPointer];
+      nextInterval = array[nextPointer];
+    } else {
+      currPointer++;
+      nextPointer++;
+      currInterval = array[currPointer];
+      nextInterval = array[nextPointer];
+    }
+  }
+
   return array;
 }
 
