845bf40423222e7458aa503fd80a044b4556c81b
Ozal Khan <ozalkhan196@gmail.com>
2025-03-03 16:58:45 -0500

    chore: setup min char for words file

---
+++ medium-problems/min-char-for-words.ts
@@ -0,0 +1,9 @@
+export function minimumCharactersForWords(words: string[]) {
+  // Write your code here.
+  return [];
+}
+
+const words = ['this', 'that', 'did', 'deed', 'them!', 'a'];
+const expected = ['t', 't', 'h', 'i', 's', 'a', 'd', 'd', 'e', 'e', 'm', '!'];
+
+console.log(minimumCharactersForWords(words));
