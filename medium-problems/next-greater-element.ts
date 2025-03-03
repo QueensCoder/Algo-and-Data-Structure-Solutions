3e808b2c1bad7828f6d09efc350cd1071202b970
Ozal Khan <ozalkhan196@gmail.com>
2025-03-03 17:56:04 -0500

    chore: setup next greater elem

---
+++ medium-problems/next-greater-element.ts
@@ -0,0 +1,11 @@
+// brute force approach O(n^2)
+export function nextGreaterElement(array: number[]) {
+  const nextGreaterArr = array.map((num) => {
+    console.log(num);
+  });
+  return [];
+}
+
+const inputArr = [2, 5, -3, -4, 6, 7, 2];
+
+console.log(nextGreaterElement(inputArr));
