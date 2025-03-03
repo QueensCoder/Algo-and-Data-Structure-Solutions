7ccc2202cc9286dc73a4eee0b35b0f5791e0b789
Ozal Khan <ozalkhan196@gmail.com>
2025-03-03 17:18:19 -0500

    feat: solve min char for words optimally

--- medium-problems/min-char-for-words.ts
+++ medium-problems/min-char-for-words.ts
@@ -1,6 +1,32 @@
 export function minimumCharactersForWords(words: string[]) {
-  // Write your code here.
-  return [];
+  const minCharsArray: string[] = [];
+  const minCharHash: { [key: string]: number } = {};
+  words.forEach((word) => {
+    // iterate over each word and hash each char with a count
+    let perWordHash: { [key: string]: number } = {};
+    word.split('').forEach((char) => {
+      if (perWordHash[char]) perWordHash[char]++;
+      else perWordHash[char] = 1;
+    });
+
+    // determine the max count for each char such that each word can be made
+    Object.entries(perWordHash).forEach(([char, count]) => {
+      if (minCharHash[char]) {
+        let currentMax = minCharHash[char];
+        minCharHash[char] = Math.max(count, currentMax);
+      } else {
+        minCharHash[char] = count;
+      }
+    });
+  });
+
+  //   push all the characters into the array using repeat
+  Object.entries(minCharHash).forEach(([char, count]) =>
+    minCharsArray.push(char.repeat(count)),
+  );
+
+  //   split characters like tt to t t by join all then split
+  return minCharsArray.join('').split('');
 }
 
 const words = ['this', 'that', 'did', 'deed', 'them!', 'a'];
