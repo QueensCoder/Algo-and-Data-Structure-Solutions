f45ec7fbd1634e74e309e3f2bc76baa4729c628a
Ozal Khan <ozalkhan196@gmail.com>
2025-03-04 13:15:13 -0500

    chore: setup brute force approach

--- medium-problems/sweet-and-savory.ts
+++ medium-problems/sweet-and-savory.ts
@@ -1,5 +1,29 @@
 export function sweetAndSavory(dishes: number[], target: number) {
-  return [];
+  dishes.sort((a, b) => a - b);
+  console.log(dishes);
+  let pointerOne = 0;
+  let pointerTwo = dishes.length - 1;
+
+  const result = [0, 0];
+  //   sweet is negative , savory is positive, result must include one of each
+  //   exit if all sweet or all savory
+  if (!dishes.some((dish) => dish < 0)) return result;
+  else if (!dishes.some((dish) => dish > 0)) return result;
+
+  const sweetDishes = dishes.filter((dish) => dish < 0);
+  const savoryDishes = dishes.filter((dish) => dish > 0);
+
+  for (let i = 0; i < sweetDishes.length; i++) {
+    let sweetDish = sweetDishes[i];
+
+    for (let j = 0; j < savoryDishes.length; j++) {
+      let savoryDish = savoryDishes[j];
+
+      const currentTarget = result.reduce((prev, curr) => (prev += curr));
+      const potentialTarget = sweetDish + savoryDish;
+      console.log(currentTarget, potentialTarget, sweetDish, savoryDish);
+    }
+  }
 }
 
 const dishes = [-3, -5, 1, 7];
